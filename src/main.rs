ede1fe590149234b6a7ffdf5995cdc12c4931391
Immortalsonly <remco.eijsackers@gmail.com>
2021-11-14 17:00:46 +0100

    wip intourl

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use structopt::StructOpt;
+use std::error::Error;
+use url::{Url, ParseError};
+
+trait IntoUrl {
+    fn into_url(self) -> String;
+}
+
+#[derive(StructOpt)]
+#[derive(Debug)]
+struct Cli {
+    /// The pattern to look for
+    pub url: String,
+}
+
+impl IntoUrl for Cli {
+    fn into_url(self) -> String
+    {self.url}
+}
+
+#[derive(StructOpt)]
+struct Rp {
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    response: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+    let ur = Cli::into_url(args);
+    let resp = reqwest::blocking::get(args)?
+        .json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+    Ok(())
+}
