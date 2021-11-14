000d35c6f25356e34f88baf299a1014b0c667653
Immortalsonly <remco.eijsackers@gmail.com>
2021-11-14 19:59:28 +0100

    calling api from cli, printing output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 use structopt::StructOpt;
-use std::error::Error;
-use url::{Url, ParseError};
 
 trait IntoUrl {
     fn into_url(self) -> String;
@@ -10,20 +8,12 @@ trait IntoUrl {
 #[derive(StructOpt)]
 #[derive(Debug)]
 struct Cli {
-    /// The pattern to look for
     pub url: String,
 }
 
 impl IntoUrl for Cli {
     fn into_url(self) -> String
     {self.url}
-}
-
-#[derive(StructOpt)]
-struct Rp {
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    response: std::path::PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
