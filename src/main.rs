70e444b495c39850b80b2e0ec8a56ec53983a5e5
Immortalsonly <remco.eijsackers@gmail.com>
2021-12-29 21:06:34 +0100

    handling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use std::io::Read;
 mod request;
 use request::mrequest;
 use request::prequest;
+use request::resobj;
+
+pub fn rqesthandler(reqtpe: String, pattern: String) -> resobj {
+    let dt: resobj = mrequest(pattern).unwrap();
+    println!("Status: {}", dt.status);
+    println!("Headers:\n{:#?}", dt.headers);
+    println!("Body:\n{}", dt.resbody);
+    return dt
+}
 
 fn main() {
     let reqtype = std::env::args().nth(1).expect("no request type / url given");
@@ -9,8 +18,8 @@ fn main() {
     let get = "get";
     let post = "post";
     let outcome = match reqtype{
-        get => mrequest(pattern),
-        post => prequest(pattern),
+        get => rqesthandler(get,pattern),
+        post => rqesthandler(post,pattern),
     };
     
 }
--- src/request.rs
+++ src/request.rs
@@ -1,28 +1,57 @@
 use std::io::Read;
 
-
-pub fn mrequest(pattern:String) -> Result<(), Box<dyn std::error::Error>> {
-    let pattern = pattern;
-    let mut res = reqwest::blocking::get(pattern)?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
-
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
-
-    Ok(())
+pub struct resobj {
+    pub status: String,
+    pub headers: String,
+    pub resbody: String,
 }
 
-pub fn prequest(pattern:String) -> Result<(), Box<dyn std::error::Error>> {
+pub fn build_resobj(status: String, headers: String, resbody: String) -> resobj {
+    resobj {
+        status : status,
+        headers : headers,
+        resbody: resbody
+    }
+}
+
+pub fn mrequest(pattern:String) -> Result<resobj, Box<dyn std::error::Error>> {
+    let pattern = pattern;
+    let mut res = reqwest::blocking::get(pattern)?;
+    let mut body = String::new();
+    let mut stcode = String::new();
+    let mut hdr = String::new();
+    
+    res.read_to_string(&mut body)?;
+    res.read_to_string(&mut stcode)?;
+    res.read_to_string(&mut hdr)?;
+    
+    let rsp = build_resobj(stcode, hdr, body);
+
+    //println!("Status: {}", res.status());
+    //println!("Headers:\n{:#?}", res.headers());
+    //println!("Body:\n{}", body);
+
+    Ok(rsp)
+}
+
+pub fn prequest(pattern:String) -> Result<resobj, Box<dyn std::error::Error>> {
     let pattern = pattern;
     let mut res = reqwest::blocking::get(pattern)?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
+    let mut body = String::new();
+    let mut stcode = String::new();
+    let mut hdr = String::new();
+    
+    res.read_to_string(&mut body)?;
+    res.read_to_string(&mut stcode)?;
+    res.read_to_string(&mut hdr)?;
+    
+    let rsp = build_resobj(stcode, hdr, body);
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
+    //println!("Status: {}", res.status());
+    //println!("Headers:\n{:#?}", res.headers());
+    //println!("Body:\n{}", body);
 
-    Ok(())
+    Ok(rsp)
 }
